eb236365c7484828c6bd22da375bb96c0ea5558b
Ryan <rsul.dev@me.com>
2015-10-20 15:52:36 -0400

    Begin list implementation

--- js-lib/types/list.js
+++ js-lib/types/list.js
@@ -1,3 +1,13 @@
 function AIList() {
-    return '(*list-for-runtime* ' + argStr + ')'
+    var args = [];
+    for (var k in arguments) {
+        var v = arguments[k];
+
+        if (typeof v == 'string') {
+            args.push("'" + v + "'")
+        } else if (typeof v == 'number') {
+            args.push(v)
+        }
+    }
+    return '(*list-for-runtime* ' + args.join(' ') + ')'
 }
