350d08bac82a8a2e5eb4279c070a4e5495bef490
Ryan <rsul.dev@me.com>
2015-10-20 16:12:54 -0400

    Convert AIColor to object

--- js-lib/types/color.js
+++ js-lib/types/color.js
@@ -1,4 +1,8 @@
 function AIColor(r, g, b, a) {
     if (typeof a == 'undefined') { a = 255; }
-    return '(make-color ' + AIList(r, g, b, a) + ')'
+    this.components = [r, g, b, a];
 }
+
+AIColor.prototype.toString = function() {
+    return '(make-color ' + new AIList(this.r, this.b, this.g, this.a) + ')';
+};
