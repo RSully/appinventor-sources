f74eb9a932f9f307b63912022c6e685240857a68
Ryan <rsul.dev@me.com>
2015-10-20 15:49:27 -0400

    Fix indent in my files

--- js-lib/types/color.js
+++ js-lib/types/color.js
@@ -1,4 +1,4 @@
 function AIColor(r, g, b, a) {
-  if (typeof a == 'undefined') { a = 255; }
-  return '(make-color '+AIList(r, g, b, a) + ')'
+    if (typeof a == 'undefined') { a = 255; }
+    return '(make-color '+AIList(r, g, b, a) + ')'
 }
--- js-lib/types/list.js
+++ js-lib/types/list.js
@@ -1,3 +1,3 @@
 function AIList() {
-  return '(*list-for-runtime* )'
+    return '(*list-for-runtime* ' + argStr + ')'
 }
