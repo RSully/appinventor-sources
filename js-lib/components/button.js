b767ef905e7b01be79bdb05889eec5d2c2d3c596
Ryan <rsul.dev@me.com>
2015-10-29 13:55:39 -0400

    Fix indentation in components, rename Button to AIButton

--- js-lib/components/button.js
+++ js-lib/components/button.js
@@ -1,39 +1,40 @@
-var Button = function() {
-  var name = 'Button'+(Object.keys(Button.instances).length+(new Date()).getSeconds());
-  AppInventor.sendEval("(add-component Screen1 Button "+name+")");
-  AppInventor.sendEval("(set-and-coerce-property! '"+name+" 'Text \"New Button\" 'text)");
-  //if (AppInventor.getEval(name).indexOf('@') != -1) success!
-  //if (arguments.length == 2) use later for parent component
-  EventfulObject.call(this);
-  this.type = "Button";
-  this.name = name;
-  this.init_props(["BackgroundColor","Enabled","FontBold","FontItalic","FontSize","FontTypeface","Height","Image","Shape","ShowFeedback","Text","TextAlignment","TextColor","Visible","Width"]);
-  this.init_events(["Click","GotFocus","LongClick","LostFocus","TouchDown","TouchUp"]);
-  Button.instances[name] = this;
+var AIButton = function() {
+    var name = 'Button'+(Object.keys(Button.instances).length+(new Date()).getSeconds());
+    AppInventor.sendEval("(add-component Screen1 Button "+name+")");
+    AppInventor.sendEval("(set-and-coerce-property! '"+name+" 'Text \"New Button\" 'text)");
+    //if (AppInventor.getEval(name).indexOf('@') != -1) success!
+    //if (arguments.length == 2) use later for parent component
+    EventfulObject.call(this);
+
+    this.type = "Button";
+    this.name = name;
+    this.init_props(["BackgroundColor", "Enabled", "FontBold", "FontItalic", "FontSize", "FontTypeface", "Height", "Image", "Shape", "ShowFeedback", "Text", "TextAlignment", "TextColor", "Visible", "Width"]);
+    this.init_events(["Click","GotFocus","LongClick","LostFocus","TouchDown","TouchUp"]);
+    Button.instances[name] = this;
 };
-Button.prototype = Object.create(EventfulObject.prototype);
-Button.prototype.init_props = function(props) {
-	var user = this;
+AIButton.prototype = Object.create(EventfulObject.prototype);
+AIButton.prototype.init_props = function(props) {
+    var user = this;
     props.forEach(function(prop) {
-    	var type = _.types[_.props.indexOf(prop)];
+        var type = _.types[_.props.indexOf(prop)];
         Object.defineProperty(user, prop, {
-          get: function(){
-            return AppInventor.getEval("("+this.name+":"+prop+")");
-          },
-          set: function(value){
-            //switch (type) {}
-            //AppInventor.getEval('(make-color (*list-for-runtime* 0 0 0 255))')
-            AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")")
-     	  }
+            get: function(){
+                return AppInventor.getEval("("+this.name+":"+prop+")");
+            },
+            set: function(value){
+                //switch (type) {}
+                //AppInventor.getEval('(make-color (*list-for-runtime* 0 0 0 255))')
+                AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")")
+            }
         });
     });
 };
-Button.prototype.init_events = function(events) {
-	var user = this;
-	events.forEach(function(event) {
-    	AppInventor.sendEval('(define-event '+user.name+' '+event+'()(set-this-form)\
-    	((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+event+'\', \''+user.name+'\')" #!null))');
+AIButton.prototype.init_events = function(events) {
+    var user = this;
+    events.forEach(function(event) {
+        AppInventor.sendEval('(define-event '+user.name+' '+event+'()(set-this-form)\
+            ((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+event+'\', \''+user.name+'\')" #!null))');
     });
 };
-Button.prototype.constructor = Button;
-Button.instances = Object();
+AIButton.prototype.constructor = AIButton;
+AIButton.instances = Object();
--- js-lib/components/screen.js
+++ js-lib/components/screen.js
@@ -1,40 +1,41 @@
 var AIScreen = function() {
-  var name = "Screen1"
-  EventfulObject.call(this);
-  this.type = "AIScreen";
-  this.name = name;
-  this.init_props(["AboutScreen","AlignHorizontal","AlignVertical","BackgroundColor","AppName","BackgroundImage","CloseScreenAnimation","Height","Icon","OpenScreenAnimation","ScreenOrientation","Scrollable","ShowStatusBar","Sizing","TitleVisible","VersionCode","VersionName","Width"]);
-  this.init_events(["BackPressed","ErrorOccurred","Initialize","OtherScreenClosed","ScreenOrientationChanged"]);
-  AIScreen.instances[name] = this;
+    var name = "Screen1"
+    EventfulObject.call(this);
+
+    this.type = "AIScreen";
+    this.name = name;
+    this.init_props(["AboutScreen", "AlignHorizontal", "AlignVertical", "BackgroundColor", "AppName", "BackgroundImage", "CloseScreenAnimation", "Height", "Icon", "OpenScreenAnimation", "ScreenOrientation", "Scrollable", "ShowStatusBar", "Sizing", "TitleVisible", "VersionCode", "VersionName", "Width"]);
+    this.init_events(["BackPressed", "ErrorOccurred", "Initialize", "OtherScreenClosed", "ScreenOrientationChanged"]);
+    AIScreen.instances[name] = this;
 };
 AIScreen.prototype = Object.create(EventfulObject.prototype);
 _.extend(AIScreen.prototype, {
-  get Title () {
-    return AppInventor.getEval(this.name+":Title");
-  },
-  set Title (value) {
-    AppInventor.sendEval('('+this.name+':setTitle "'+value+'")');
-  }
+    get Title () {
+        return AppInventor.getEval(this.name+":Title");
+    },
+    set Title (value) {
+        AppInventor.sendEval('('+this.name+':setTitle "'+value+'")');
+    }
 })
 AIScreen.prototype.init_props = function(props) {
-	var user = this;
+    var user = this;
     props.forEach(function(prop) {
-    	var type = _.types[_.props.indexOf(prop)];
+        var type = _.types[_.props.indexOf(prop)];
         Object.defineProperty(user, prop, {
-          get: function(){
-            return AppInventor.getEval("(get-property '"+this.name+" '"+prop+")");
-          },
-          set: function(value){
-            AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")")
-     	    }
+            get: function(){
+                return AppInventor.getEval("(get-property '"+this.name+" '"+prop+")");
+            },
+            set: function(value){
+                AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")")
+            }
         });
     });
 };
 AIScreen.prototype.init_events = function(events) {
-	var user = this;
-	events.forEach(function(event) {
-    	AppInventor.sendEval('(define-event '+user.name+' '+event+'()(set-this-form)\
-    	((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+event+'\', \''+user.name+'\')" #!null))');
+    var user = this;
+    events.forEach(function(event) {
+        AppInventor.sendEval('(define-event '+user.name+' '+event+'()(set-this-form)\
+            ((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+event+'\', \''+user.name+'\')" #!null))');
     });
 };
 AIScreen.prototype.constructor = AIScreen;
