8fe7c99f6e1a0b7e3936e4bf6b24e601489640a1
Ryan <rsul.dev@me.com>
2015-10-30 11:50:01 -0400

    Update app.js to use proper button name

--- js-lib/app.js
+++ js-lib/app.js
@@ -2,8 +2,10 @@ var screen1 = new AIScreen();
 screen1.Title = document.title;
 screen1.ShowStatusBar = new AIBool(0);
 
-var button1 = new Button();
+var button1 = new AIButton();
 button1.Text = new AIText('awesome');
 button1.on('Click', function() {
   alert('dude!!');
 });
+
+// var canvas = new AICanvas();
