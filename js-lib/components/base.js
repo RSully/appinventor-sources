2d15a5ff4b611161d80bf7e86baf402b66bf44aa
Ryan <rsul.dev@me.com>
2015-10-30 12:05:07 -0400

    Begin rewriting base/button files with new concept

---
+++ js-lib/components/base.js
@@ -0,0 +1,113 @@
+var AIProperties = {
+    "AboutScreen": "text",
+    "ColorLeft": "number",
+    "Title": "text",
+    "AlignVertical": "number",
+    "FontItalic": "boolean",
+    "Width": "number",
+    "Animation": "boolean",
+    "TitleVisible": "boolean",
+    "Month": "any",
+    "NotifierLength": "number",
+    "TextAlignment": "number",
+    "ShowFeedback": "boolean",
+    "Picture": "text",
+    "FontBold": "boolean",
+    "Elements": "any",
+    "AppName": "text",
+    "ColorRight": "number",
+    "MinValue": "number",
+    "SelectionIndex": "number",
+    "Day": "any",
+    "Minute": "any",
+    "Icon": "text",
+    "Sizing": "any",
+    "Scrollable": "boolean",
+    "Instant": "any",
+    "ItemBackgroundColor": "number",
+    "Hour": "any",
+    "ThumbEnabled": "boolean",
+    "FontTypeface": "number",
+    "Enabled": "boolean",
+    "CloseScreenAnimation": "boolean",
+    "OpenScreenAnimation": "boolean",
+    "FontSize": "number",
+    "MonthInText": "any",
+    "BackgroundColor": "number",
+    "VersionCode": "text",
+    "TextColor": "number",
+    "ThumbPosition": "number",
+    "ElementsFromString": "text",
+    "ShowStatusBar": "boolean",
+    "Selection": "any",
+    "Prompt": "boolean",
+    "ShowFilterBar": "boolean",
+    "Text": "text",
+    "Image": "text",
+    "ScreenOrientation": "any",
+    "BackgroundImage": "text",
+    "MaxValue": "number",
+    "Height": "number",
+    "Visible": "boolean",
+    "Shape": "number",
+    "VersionName": "text",
+    "Year": "any",
+    "ItemTextColor": "number",
+    "AlignHorizontal": "number"
+};
+
+function AIBaseComponent() {
+    // Setup type + name
+    this.type = this.constructor.name;
+    AIBaseComponent.instances[this.type].push(this);
+    this.name = this.type + (AIBaseComponent.uuid++);
+
+    // Setup per-instance events
+    if (this.constructor.hasOwnProperty('events')) {
+        AIBaseComponent.setupEvents.call(this);
+    }
+};
+
+AIBaseComponent.uuid = 0;
+AIBaseComponent.instances = {};
+
+AIBaseComponent.setup = function(object) {
+    if (!this.instances.hasOwnProperty(object.name)) {
+        this.instances[object.name] = [];
+    }
+
+    // Setup inheritance automatically
+    object.prototype = Object.create(this.prototype);
+    object.prototype.constructor = object;
+
+    // Register properties
+    if (object.hasOwnProperty('properties')) {
+        this.setupProperties(object);        
+    }
+};
+
+AIBaseComponent.setupProperties = function(object) {
+    object.properties.forEach(function(property){
+        var type = AIProperties[property];
+
+        Object.defineProperty(object.prototype, property, {
+            get: function() {
+                console.log('called the getter!');
+                return 'woo return for ' + this.name;
+                //return AppInventor.getEval("("+this.name+":"+prop+")");
+            },
+            set: function(value) {
+                console.log('called the setter for ' + this.name);
+                //AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")");
+            }
+        });
+    });
+
+};
+
+AIBaseComponent.setupEvents = function() {
+    this.constructor.events.forEach(function(event){
+        //AppInventor.sendEval('(define-event '+that.name+' '+e+'()(set-this-form)\
+        //    ((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+e+'\', \''+that.name+'\')" #!null))');
+    }.bind(this));
+};
--- js-lib/components/button.js
+++ js-lib/components/button.js
@@ -1,40 +1,13 @@
-var AIButton = function() {
-    var name = 'Button'+(Object.keys(Button.instances).length+(new Date()).getSeconds());
-    AppInventor.sendEval("(add-component Screen1 Button "+name+")");
-    AppInventor.sendEval("(set-and-coerce-property! '"+name+" 'Text \"New Button\" 'text)");
-    //if (AppInventor.getEval(name).indexOf('@') != -1) success!
-    //if (arguments.length == 2) use later for parent component
-    EventfulObject.call(this);
+function AIButton() {
+    AIBaseComponent.call(this);
 
-    this.type = "Button";
-    this.name = name;
-    this.init_props(["BackgroundColor", "Enabled", "FontBold", "FontItalic", "FontSize", "FontTypeface", "Height", "Image", "Shape", "ShowFeedback", "Text", "TextAlignment", "TextColor", "Visible", "Width"]);
-    this.init_events(["Click", "GotFocus", "LongClick", "LostFocus", "TouchDown", "TouchUp"]);
-    AIButton.instances[name] = this;
+    // AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" 'Text \"New Button\" 'text)");
+    // if (AppInventor.getEval(name).indexOf('@') != -1) success!
+    // if (arguments.length == 2) use later for parent component
+    // EventfulObject.call(this);
 };
-AIButton.prototype = Object.create(EventfulObject.prototype);
-AIButton.prototype.init_props = function(props) {
-    var user = this;
-    props.forEach(function(prop) {
-        var type = _.types[_.props.indexOf(prop)];
-        Object.defineProperty(user, prop, {
-            get: function(){
-                return AppInventor.getEval("("+this.name+":"+prop+")");
-            },
-            set: function(value){
-                //switch (type) {}
-                //AppInventor.getEval('(make-color (*list-for-runtime* 0 0 0 255))')
-                AppInventor.sendEval("(set-and-coerce-property! '"+this.name+" '"+prop+" "+value+" '"+type+")")
-            }
-        });
-    });
-};
-AIButton.prototype.init_events = function(events) {
-    var user = this;
-    events.forEach(function(event) {
-        AppInventor.sendEval('(define-event '+user.name+' '+event+'()(set-this-form)\
-            ((WebViewer1:getView):evaluateJavascript "_.dispatch.emit(\''+event+'\', \''+user.name+'\')" #!null))');
-    });
-};
-AIButton.prototype.constructor = AIButton;
-AIButton.instances = Object();
+
+AIButton.properties = ["BackgroundColor", "Enabled", "FontBold", "FontItalic", "FontSize", "FontTypeface", "Height", "Image", "Shape", "ShowFeedback", "Text", "TextAlignment", "TextColor", "Visible", "Width"];
+AIButton.events = ["Click", "GotFocus", "LongClick", "LostFocus", "TouchDown", "TouchUp"];
+
+AIBaseComponent.setup(AIButton);
