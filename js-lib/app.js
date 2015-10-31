a10b124af158949f16e4c19cd377cd46a0f3d2f3
Ryan <rsul.dev@me.com>
2015-10-31 01:02:42 -0400

    Disable button1 alert for now

--- js-lib/app.js
+++ js-lib/app.js
@@ -4,8 +4,8 @@ screen1.ShowStatusBar = new AIBool(0);
 
 var button1 = new AIButton();
 button1.Text = new AIText('awesome');
-button1.on('Click', function() {
-  alert('dude!!');
-});
+// button1.on('Click', function() {
+//   alert('dude!!');
+// });
 
 // var canvas = new AICanvas();
