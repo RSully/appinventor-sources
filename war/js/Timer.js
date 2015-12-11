30e29c6def5b51cf2c099838730c1c130d11526d
Ryan <rsul.dev@me.com>
2015-12-10 19:33:12 -0500

    Convert timer to spaces

--- war/js/Timer.js
+++ war/js/Timer.js
@@ -1,34 +1,34 @@
 function Timer(settings) {
-	this.settings = settings;
-	this.timer = null;
+    this.settings = settings;
+    this.timer = null;
 
-	this.fps = settings.fps || 30;
-	this.interval = Math.floor(1000 / 30); // should this be `1000/this.fps`?
-	this.timeInit = null;
+    this.fps = settings.fps || 30;
+    this.interval = Math.floor(1000 / 30);
+    this.timeInit = null;
 }
 
 Timer.prototype = {
-	run: function() {
-		this.settings.run();
-		this.timeInit += this.interval;
+    run: function() {
+        this.settings.run();
+        this.timeInit += this.interval;
 
-		this.timer = setTimeout(
-			function() {
-				this.run()
-			}.bind(this),
-			this.timeInit - (new Date).getTime()
-		);
-	},
+        this.timer = setTimeout(
+            function() {
+                this.run()
+            }.bind(this),
+            this.timeInit - (new Date).getTime()
+        );
+    },
 
-	start: function() {
-		if (this.timer == null) {
-			this.timeInit = (new Date).getTime();
-			this.run();
-		}
-	},
+    start: function() {
+        if (this.timer == null) {
+            this.timeInit = (new Date).getTime();
+            this.run();
+        }
+    },
 
-	stop: function() {
-		clearTimeout(this.timer);
-		this.timer = null;
-	}
+    stop: function() {
+        clearTimeout(this.timer);
+        this.timer = null;
+    }
 }
