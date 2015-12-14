941a6e37ca0209dac840ecf3725a3b1a3a460eb7
Ryan <rsul.dev@me.com>
2015-12-13 23:07:13 -0500

    Make Laser extend ImageView

--- war/js/game/Laser.js
+++ war/js/game/Laser.js
@@ -1,15 +1,6 @@
 function Laser(settings) {
-    this.image = settings.image;
-    this.x = settings.x;
-    this.y = settings.y;
-    this.width = settings.width || this.image.width;
-    this.height = settings.height || this.image.height;
+    ImageView.call(this, settings);
 }
 
-Laser.prototype.draw = function(ctx) {
-    ctx.drawImage(
-        this.image,
-        this.x, this.y,
-        this.width, this.height
-    );
-};
+Laser.prototype = Object.create(ImageView.prototype);
+Laser.prototype.constructor = Laser;
